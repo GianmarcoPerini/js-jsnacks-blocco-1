35edc21c59a1c0a87bca3383b45836fc79b1ed29
GianmarcoPerini <g.marco.perini@gmail.com>
2021-08-28 12:05:05 +0200

    SOMMA CON FOR E WHILE

---
+++ main.js
@@ -0,0 +1,24 @@
+//SOMMA CON FOR E WHILE
+
+/**
+ * 
+ *
+ *   let somma = 0;
+ *
+ *   for (let i = 0; i < 5; i++){
+ *      let numeriPerSomma = parseInt(prompt("Inserisci 5 numeri")) 
+ *     somma += numeriPerSomma
+ *}
+ *
+ *
+ *   let n = 0;
+ *
+ *   while (n < 5) {
+ *      let numeriPerSomma = parseInt(prompt("Inserisci 5 numeri"))
+ *     somma += numeriPerSomma
+ *    n++
+ *}
+ *
+ *   console.log(somma);
+ *
+ */
